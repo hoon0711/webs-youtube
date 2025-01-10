9fbdd82b414868d6474af38eff4b1d2ff332b3cc
hoon0711 <doh40711@gmail.com>
2025-01-10 20:19:19 +0900

    유튜브 헤더

--- src/components/include/Header.jsx
+++ src/components/include/Header.jsx
@@ -1,80 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { CiBaseball } from "react-icons/ci";
-import { CiCoins1 } from "react-icons/ci";
-import { CiBoxes } from "react-icons/ci";
-import { CiBullhorn } from "react-icons/ci";
-import { CiCoffeeCup } from "react-icons/ci";
-import { CiDumbbell } from "react-icons/ci";
-import { CiFries } from "react-icons/ci";
-import { CiMoneyBill } from "react-icons/ci";
+import Logo from '../header/Logo';
+import Menu from '../header/Menu';
+import Sns from '../header/Sns';
 
-import { AiFillGithub } from "react-icons/ai";
-import { AiOutlineCodepen } from "react-icons/ai";
-import { AiFillYoutube } from "react-icons/ai";
-import { AiOutlineInstagram } from "react-icons/ai";
-import { Link } from 'react-router-dom';
-
-const Header = () => {
+const Header = () => {    
     return (
-        <header id='header' role='banner'>
-            <h1 className='header__logo'>
-                <Link to ='/'>
-                    <em aria-hidden='true'></em>
-                    <span>webs<br />youtube</span>
-                </Link>
-            </h1>
-
-            <nav className='header__menu'>
-                <ul className='menu'>
-                    <li className='active'><Link to='/'><CiBaseball /> 웹스토리보이</Link></li>
-                    <li><Link to='/today'><CiMoneyBill /> 추천 영상</Link></li>
-                    <li><Link to='/developer'><CiCoins1 /> 추천 개발자</Link></li>
-                    <li><Link to='/webd'><CiBoxes /> 웹디자인기능사</Link></li>
-                    <li><Link to='/website'><CiBullhorn /> 웹표준 사이트</Link></li>
-                    <li><Link to='/gsap'><CiCoffeeCup /> GSAP Parallax</Link></li>
-                    <li><Link to='/port'><CiDumbbell /> 포트폴리오 사이트</Link></li>
-                    <li><Link to='/youtube'><CiFries /> 유튜브 클론 사이트</Link></li>
-                </ul>
-                <ul className='keyword'>
-                    <li><Link to='/search/webstoryboy'>webstoryboy</Link></li>
-                    <li><Link to='/search/html'>HTML</Link></li>
-                    <li><Link to='/search/css'>CSS</Link></li>
-                    <li><Link to='/search/javascript'>JavaScript</Link></li>
-                    <li><Link to='/search/react.js'>React.js</Link></li>
-                    <li><Link to='/search/vue.js'>Vue.js</Link></li>
-                    <li><Link to='/search/next.js'>Next.js</Link></li>
-                    <li><Link to='/search/node.js'>Node.js</Link></li>
-                    <li><Link to='/search/sql'>SQL</Link></li>
-                    <li><Link to='/search/React Portfolio'>portfolio</Link></li>
-                    <li><Link to='/search/NewJeans'>music</Link></li>
-                </ul>
-            </nav>
-            
-            <div className='header__sns'>
-                <ul>
-                    <li>
-                        <a href='https://github.com/webstoryboy' rel='noopener noreferrer'>
-                            <AiFillGithub />
-                        </a>
-                    </li>
-                    <li>
-                        <a href='https://www.youtube.com/webstoryboy' rel='noopener noreferrer'>
-                            <AiFillYoutube />
-                        </a>
-                    </li>
-                    <li>
-                        <a href='https://codepen.io/webstoryboy' rel='noopener noreferrer'>
-                            <AiOutlineCodepen />
-                        </a>
-                    </li>
-                    <li>
-                        <a href='https://www.instagram.com/webstoryboy' rel='noopener noreferrer'>
-                            <AiOutlineInstagram />
-                        </a>
-                    </li>
-                </ul>
-            </div>
+        <header id='header' role='banner' >
+            <Logo />
+            <Menu />
+            <Sns />
         </header>
     )
 }
